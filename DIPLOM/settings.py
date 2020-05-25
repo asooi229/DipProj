0dd4beab3ccddf7e8ef8c9c789b89d911250390a
asooi229 <Kirill.Statkevich@mail.ru>
2020-05-25 10:49:23 +0300

    my third commit

--- DIPLOM/settings.py
+++ DIPLOM/settings.py
@@ -75,10 +75,14 @@ WSGI_APPLICATION = 'DIPLOM.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+ 'default': {
+ 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+ 'NAME': 'myprojdb',
+ 'USER': 'myprojectuser',
+ 'PASSWORD': 'password',
+ 'HOST': 'localhost',
+ 'PORT': '',
+ }
 }
 
 
